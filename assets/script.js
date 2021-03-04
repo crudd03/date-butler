a22a700538865455532be5d02d583bbe458478a4
Kayte McDonough <kaytemcdonough@gmail.com>
2021-03-04 14:40:40 -0500

    api work. was able to fetch random drink

--- assets/script.js
+++ assets/script.js
@@ -1,43 +1,80 @@
-//Kayte's Cocktail API Work
 
-$("#userSearchBtn").on("click", function() {
-    event.preventDefault();
-    userInput = $("#serSearch").val()
+//random drink
+function getRandomCocktail() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/random.php')
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log('Looks like there was a problem. Status Code: ' +
+          response.status);
+        return;
+      }
 
-    appendRecipe();
-    storeRecipes();
-})
-
-
-function appendRecipe() {
-    let queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
-
-    $.ajax9({
-        url: queryURL,
-        method: "GET"
-    }).then(function(response) {
-        console.log(response)
-        // randomly select recipe by ID; then do an API call to get the source URL of that recipe
-        if (response.reasults.length === 0) {
-            alert("uh oh!");
-            return false;
-        }
-
-        let recipeIndex = Math.floor(Math.random() * (response.results.length - 0) + 0);
-        let recipeID = response.results[recipeIndex].id;
-
-        let newQueryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007"
-
-        $.ajax({
-            url: newQueryURL,
-            method: "GET"
-        }).then(function(response){
-
-            let recipeURL = response.sourceURL;
-            
-            
-            
-
-        })
-    })
+      // Examine the text in the response
+      response.json().then(function(data) {
+        console.log(data);
+        displayRandomCocktail(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log('Fetch Error :-S', err);
+  });
 }
+
+getRandomCocktail(); 
+
+function displayRandomCocktail(cocktail) {
+    console.log(cocktail.drinks[0]);
+
+    let drinkSection = document.querySelector('#drink-section');
+
+    let drinkName = document.createElement('h2');
+    drinkName.innerHTML = cocktail.drinks[0].strDrink;
+
+    drinkSection.appendChild(drinkName);
+}
+
+
+
+// $("#userSearchBtn").on("click", function() {
+//     event.preventDefault();
+//     userInput = $("#serSearch").val()
+
+//     appendRecipe();
+//     storeRecipes();
+// })
+
+
+// function appendRecipe() {
+//     let queryURL = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita"
+
+//     $.ajax9({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function(response) {
+//         console.log(response)
+//         // randomly select recipe by ID; then do an API call to get the source URL of that recipe
+//         if (response.reasults.length === 0) {
+//             alert("uh oh!");
+//             return false;
+//         }
+
+//         let recipeIndex = Math.floor(Math.random() * (response.results.length - 0) + 0);
+//         let recipeID = response.results[recipeIndex].id;
+
+//         let newQueryURL = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007"
+
+//         $.ajax({
+//             url: newQueryURL,
+//             method: "GET"
+//         }).then(function(response){
+
+//             let recipeURL = response.sourceURL;
+            
+
+            
+
+//         })
+//     })
+// }
